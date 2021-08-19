479917957f6b9b2808df4f67a2b804d38ad69b5b
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-08-19 19:59:37 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,5 @@
 const inputRef = document.querySelector('#name-input');
 const outputRef = document.querySelector('#name-output');
-console.log(inputRef);
-console.log(outputRef);
 
 inputRef.addEventListener('input', getvalue)
 function getvalue({target}) {
